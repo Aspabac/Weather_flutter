3e1119e8a53ad05b965b35fdd3ac3189303c1d9f
Daigomizuki <daigo3262@gmail.com>
2020-11-05 14:05:37 +0900

    experiment using API with sample code

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_flutter/services/location.dart';
+import 'package:http/http.dart' as http;
 
 
 class LoadingScreen extends StatefulWidget {
@@ -10,8 +11,9 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
-    getLocation();
     super.initState();
+    getLocation();
+    print('this code is triggered');
   }
 
   void getLocation() async {
@@ -21,8 +23,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
   }
 
+  void getData() async{
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    }else {
+      print(response.statusCode);
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
